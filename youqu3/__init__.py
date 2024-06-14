07f3215617894042583c8c63ea29ee7e2ea65bda
mikigo <47137003+mikigo@users.noreply.github.com>
2024-06-15 01:58:49 +0800

    更新 __init__.py

--- youqu3/__init__.py
+++ youqu3/__init__.py
@@ -6,5 +6,6 @@ from funnylog import logger as logger
 
 from youqu3._setting import setting as setting
 from youqu3.sleepx import sleep
+from youqu3.__version__ import __version__ as version
 
-__all__ = ['setting', 'log', 'logger', 'sleep']
+__all__ = ['setting', 'log', 'logger', 'sleep', 'version']
