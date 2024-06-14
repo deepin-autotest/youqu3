8247740946f2f395cd8e671f1b33416e848946c1
mikigo <47137003+mikigo@users.noreply.github.com>
2024-06-15 02:31:13 +0800

    更新 mousekey.py

--- youqu3/mousekey.py
+++ youqu3/mousekey.py
@@ -7,37 +7,3 @@ except ImportError:
 
 if HAS_MOUSEKEY is False:
     raise ImportError("youqu-mousekey module is required, try 'pip install youqu-mousekey'")
-
-
-class MouseKeyChainMixin:
-
-    x = None
-    y = None
-
-    @classmethod
-    def _check_xy(cls):
-        if cls.x is None and cls.y is None:
-            raise ValueError
-
-    @classmethod
-    def click(cls):
-        cls._check_xy()
-        MouseKey.click(cls.x, cls.y)
-        return cls
-
-    @classmethod
-    def right_click(cls):
-        cls._check_xy()
-        MouseKey.right_click(cls.x, cls.y)
-        return cls
-
-    @classmethod
-    def double_click(cls):
-        cls._check_xy()
-        MouseKey.double_click(cls.x, cls.y)
-        return cls
-
-    @classmethod
-    def center(cls):
-        cls._check_xy()
-        return cls.x, cls.y
